4d93651822c7c797dbd1ce616e0c2cf7600c92bf
SnowmanV2 <55754712+SnowmanV2@users.noreply.github.com>
2019-10-31 20:27:55 +0300

    8-5 another solution

--- task8-5/src/ru/tinkoff/task5.kt
+++ task8-5/src/ru/tinkoff/task5.kt
@@ -13,24 +13,11 @@ package ru.tinkoff
 // Проблема - forEach - inline функция, поэтому return выйдет из функции, и println("boom") не сработает
 // Если бы forEach была бы не inline функцией, то return в принципе нельзя было бы использовать
 // Решение - перенести лист и ф-ию forEach в отдельную функцию, а println("boom") выполнить в main():
-fun countDown() {
+fun main() {
     listOf("three", "two", "one").forEach {
-        if (it == "one") {
-            return
+        if(it == "one") {
+            return println("Boom")
         }
         println(it)
     }
 }
-fun main() {
-    countDown()
-    println("boom!")
-}
-
-// Другое решение (однако, меняющее лямбду)
-/* fun main() {
-    listOf("three", "two", "one").forEach {
-        if (it != "one")
-            println(it)
-    }
-    println("boom!")
-} */
