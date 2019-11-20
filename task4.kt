e2a3775ca4947d91fa4a2c008482879657735821
SnowmanV2 <SnowmanV2@yandex.ru>
2019-11-20 17:43:06 +0300

    task12-4

--- task4.kt
+++ task4.kt
@@ -1,11 +1,4 @@
 fun main() {
     val names = listOf("Хрюша", "Степаша", "Филя", "Гуля")
-    var maxLength = names[0].length
-    do {
-        val maybeMaxLength = names.find { it.length > maxLength }?.length
-        if (maybeMaxLength == null)
-            return println(maxLength)
-        else
-            maxLength = maybeMaxLength
-    } while (true)
+    println(names.maxBy {it.length})
 }
